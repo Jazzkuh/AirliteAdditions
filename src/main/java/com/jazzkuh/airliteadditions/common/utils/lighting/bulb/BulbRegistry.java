943857b8a20f7dfe833c2c450bebf0d38ece8343
Jazz <jazzbauer77@gmail.com>
2024-01-27 16:41:42 +0100

    Bulbreg update

--- src/main/java/com/jazzkuh/airliteadditions/common/utils/lighting/bulb/BulbRegistry.java
+++ src/main/java/com/jazzkuh/airliteadditions/common/utils/lighting/bulb/BulbRegistry.java
@@ -11,28 +11,28 @@ public class BulbRegistry {
 	private static final List<Bulb> bulbs = new ArrayList<>();
 
 	static {
-		bulbs.add(new Bulb("studio_right", "192.168.178.192", "studio", "right", "purple", "indigo"));
-		bulbs.add(new Bulb("studio_left", "192.168.178.111", "studio", "left", "green", "indigo"));
-		bulbs.add(new Bulb("studio_led_strip", "192.168.178.205", "studio", "led_strip", "green", "magenta"));
-		bulbs.add(new Bulb("studio_led_strip2", "192.168.178.23"));
-		bulbs.add(new Bulb("studio_back", "192.168.178.247", "studio", "back", "green", "indigo"));
+		bulbs.add(new Bulb("studio_right", "192.168.1.87", "studio", "right", "purple", "indigo"));
+		bulbs.add(new Bulb("studio_left", "192.168.1.77", "studio", "left", "green", "indigo"));
+		bulbs.add(new Bulb("studio_led_strip", "192.168.1.92", "studio", "led_strip", "green", "magenta"));
+		bulbs.add(new Bulb("studio_led_strip2", "192.168.1.83"));
+		bulbs.add(new Bulb("studio_back", "192.168.1.81", "studio", "back", "green", "indigo"));
 
-		bulbs.add(new Bulb("living_hanging_one", "192.168.178.134", "living", "hanging", "scarlet"));
-		bulbs.add(new Bulb("living_hanging_two", "192.168.178.141", "living", "hanging", "scarlet"));
-		bulbs.add(new Bulb("living_hanging_three", "192.168.178.185", "living", "hanging", "scarlet"));
+		bulbs.add(new Bulb("living_hanging_one", "192.168.1.84", "living", "hanging", "scarlet"));
+		bulbs.add(new Bulb("living_hanging_two", "192.168.1.94", "living", "hanging", "scarlet"));
+		bulbs.add(new Bulb("living_hanging_three", "192.168.1.79", "living", "hanging", "scarlet"));
 
-		bulbs.add(new Bulb("living_table_one", "192.168.178.68", "living", "hanging", "scarlet"));
-		bulbs.add(new Bulb("living_table_two", "192.168.178.135", "living", "hanging", "scarlet"));
+		bulbs.add(new Bulb("living_table_one", "192.168.1.86", "living", "hanging", "scarlet"));
+		bulbs.add(new Bulb("living_table_two", "192.168.1.93", "living", "hanging", "scarlet"));
 
-		bulbs.add(new Bulb("living_yellow_lamp", "192.168.178.55", "living", "yellow_lamp", "scarlet"));
-		bulbs.add(new Bulb("living_standing", "192.168.178.235", "living", "standing", "scarlet"));
+		bulbs.add(new Bulb("living_yellow_lamp", "192.168.1.91", "living", "yellow_lamp", "scarlet"));
+		bulbs.add(new Bulb("living_standing", "192.168.1.88", "living", "standing", "scarlet"));
 
-		bulbs.add(new Bulb("kitchen_one", "192.168.178.32", "kitchen", "one", "warm_white"));
-		bulbs.add(new Bulb("kitchen_two", "192.168.178.61", "kitchen", "two", "warm_white"));
-		bulbs.add(new Bulb("kitchen_three", "192.168.178.173", "kitchen", "three", "warm_white"));
+		bulbs.add(new Bulb("kitchen_one", "192.168.1.89", "kitchen", "one", "warm_white"));
+		bulbs.add(new Bulb("kitchen_two", "192.168.1.90", "kitchen", "two", "warm_white"));
+		bulbs.add(new Bulb("kitchen_three", "192.168.1.80", "kitchen", "three", "warm_white"));
 
-		bulbs.add(new Bulb("cabinet_one", "192.168.178.51", "living", "cabinet", "warm_white"));
-		bulbs.add(new Bulb("cabinet_two", "192.168.178.97", "living", "cabinet", "warm_white"));
+		bulbs.add(new Bulb("cabinet_one", "192.168.1.85", "living", "cabinet", "warm_white"));
+		bulbs.add(new Bulb("cabinet_two", "192.168.1.82", "living", "cabinet", "warm_white"));
 	}
 
 	public static List<Bulb> getAllBulbs() {
